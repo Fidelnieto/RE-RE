f6de5eed760fe0f982c3c873de17170b8db340bd
Fidel Nieto <nietofidel25@gmail.com>
2025-08-30 22:39:16 -0400

    adding some localStorage simple function, best time now appear with the besttime, a little and ugly list of times appear too

--- ReactionTest/script.js
+++ ReactionTest/script.js
@@ -1,36 +1,96 @@
 const area = document.getElementById("area");
 const buttonStart = document.getElementsByClassName("start");
 const buttonReset = document.getElementsByClassName("restart");
+const scoreList = document.getElementById("score");
+const bestTime = document.getElementById("bestTime");
+const clearButton = document.getElementsByClassName("clearStorage");
+let best;
 let state = "idle";
 let time = 0;
+let timeOutId = null;
+
+scoreList.innerHTML = localStorage.getItem("scoresHTML") || "";
+bestTime.innerHTML = localStorage.getItem("bestTimeHTML") || `Best Time: `;
 
 buttonStart[0].addEventListener("click", () => {
   buttonStart[0].style.display = "none";
-  state = "waiting";
   const dot = document.createElement("div");
   let t0 = performance.now();
-  const randomNumber = Math.floor(Math.random());
+  const randomNumber = Math.floor(Math.random() * (4000 - 800) + 800);
 
-  setTimeout(() => {
-    dot.style.backgroundColor = "red";
-    dot.style.width = "300px";
-    dot.style.height = "300px";
+  if (state == "idle") {
+    state = "waiting";
+    console.log(state);
 
-    area.appendChild(dot);
-    buttonReset[0].style.display = "block";
-    state = "ready";
-  }, 2000);
+    timeOutId = setTimeout(() => {
+      state = "ready";
+      dot.style.backgroundColor = "red";
+      dot.style.width = "300px";
+      dot.style.height = "300px";
+
+      area.appendChild(dot);
+      buttonReset[0].style.display = "block";
+    }, randomNumber);
+    console.log(randomNumber);
+  }
 
   dot.addEventListener("mousedown", () => {
-    time = `${((performance.now() - t0 - 2000) / 1000).toFixed(3)}sg`;
-    console.log(time);
+    if (time == "") {
+      time = `${((performance.now() - t0 - randomNumber) / 1000).toFixed(3)}sg`;
+      console.log(time);
+
+      const score = document.createElement("li");
+      score.innerHTML = `${time}`;
+      score.classList = "scoreList";
+
+      scoreList.appendChild(score);
+
+      localStorage.setItem("scoresHTML", scoreList.innerHTML);
+
+      if (best == 0 || best == "" || best == undefined || best == null) {
+        best = time;
+        bestTime.innerHTML = `Best time: ${best}`;
+        localStorage.setItem("bestTimeHTML", bestTime.innerHTML);
+      }
+
+      if (best > time) {
+        best = time;
+        bestTime.innerHTML = `Best time: ${best}`;
+        localStorage.setItem("bestTimeHTML", bestTime.innerHTML);
+      }
+    } else {
+      alert("reset pls");
+    }
   });
+});
+
+area.addEventListener("click", () => {
+  if (state == "waiting" && timeOutId != null) {
+    clearTimeout(timeOutId);
+    buttonReset[0].style.display = "none";
+    buttonStart[0].style.display = "block";
+    t0 = 0;
+    time = 0;
+    score = 0;
+    state = "idle";
+    alert("dont cheat, lol");
+  }
 });
 
 buttonReset[0].addEventListener("click", () => {
   area.innerHTML = "";
+
   buttonReset[0].style.display = "none";
   buttonStart[0].style.display = "block";
   t0 = 0;
   time = 0;
+  score = 0;
+  state = "idle";
+});
+
+clearButton[0].addEventListener("click", () => {
+  bestTime.innerHTML = "Best time: ";
+  scoreList.innerHTML = "";
+  localStorage.setItem("scoresHTML", scoreList.innerHTML);
+  localStorage.setItem("bestTimeHTML", bestTime.innerHTML);
 });
